860fc1dd69ff94b9e706e8a981d2509cef1e6c08
Danila <AngelofDest1ny@yandex.ru>
2023-10-19 11:41:26 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let wordlength = 10;
+  let sum = wordlength;
+  let decodeString = '';
+  let space = '**********'
+  let dots = '10'
+  let dashes = '11'
+  for(let i = 0; i < expr.length; i++) {
+    if(i % 10 == 0) {
+      let word = expr.slice(i, sum)
+      if(word === space) {
+        decodeString += ' '
+      } else {
+        let morseDigit = '';
+        for(let j = 0; j < wordlength; j++) {
+          if(j % 2 == 0) {
+            if(word.slice(j, j + 2).includes(dashes)) {
+              morseDigit += '-'
+            } else if (word.slice(j, j + 2).includes(dots)) {
+              morseDigit += '.'
+            } else {
+              morseDigit += ''
+            }
+          }
+        }
+        for(let key in MORSE_TABLE) {
+          if(morseDigit == key) {
+            decodeString += MORSE_TABLE[key]
+          }
+        }
+      }
+      sum += wordlength
+    }
+  }
+  return decodeString
 }
 
 module.exports = {
